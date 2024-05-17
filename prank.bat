2fcbc7141d0c751c23409d57efeb87b4d853f476
hailongcoding <156517170+hailongcoding@users.noreply.github.com>
2024-05-17 21:09:04 +0700

    Create prank.bat

---
+++ prank.bat
@@ -0,0 +1,82 @@
+@echo OFF
+call :load
+
+:load
+    color 6
+    echo work starting ...
+    timeout /t 5 /nobreak > nul
+    echo downloading code ...
+    timeout /t 10 /nobreak > nul 
+    echo excuting code ...
+    timeout /t 2 /nobreak > nul 
+    call :hack
+
+:hack
+    cls
+    color c
+    call :jumpscare
+    call :flash
+    call :flash
+    timeout /t 15 /nobreak > nul
+    call :prank
+    cmd /k >nul 
+
+    :flash
+        color fc
+        timeout /t 1 /nobreak >nul
+        color cf
+        timeout /t 1 /nobreak >nul
+        color fc
+        timeout /t 1 /nobreak >nul
+        color cf
+        timeout /t 1 /nobreak >nul
+        color fc
+        timeout /t 1 /nobreak >nul
+        color cf
+
+    :prank
+        cls
+        echo WARNING
+        timeout /t 2 /nobreak > nul
+        echo All your data been CORUPTED.
+        timeout /t 2 /nobreak > nul
+        echo If you leave the datas will imedietly send to the hacker.
+        timeout /t 2 /nobreak > nul
+        echo But theres a DEAL.
+        timeout /t 2 /nobreak > nul
+        echo if you send money to this website your data will be put back into place .
+        timeout /t 2 /nobreak > nul
+        echo https://sites.google.com/view/1241116/121241116
+        call :forever
+            
+            :forever
+            pause > nul
+            call :forever
+    :jumpscare                                                                  
+        echo DDDDDDDDDDDDD      IIIIIIIIIIEEEEEEEEEEEEEEEEEEEEEE      !!!  !!! 
+        echo D::::::::::::DDD   I::::::::IE::::::::::::::::::::E     !!:!!!!:!!
+        echo D:::::::::::::::DD I::::::::IE::::::::::::::::::::E     !:::!!:::!
+        echo DDD:::::DDDDD:::::DII::::::IIEE::::::EEEEEEEEE::::E     !:::!!:::!
+        echo   D:::::D    D:::::D I::::I    E:::::E       EEEEEE     !:::!!:::!
+        echo   D:::::D     D:::::DI::::I    E:::::E                  !:::!!:::!
+        echo   D:::::D     D:::::DI::::I    E::::::EEEEEEEEEE        !:::!!:::!
+        echo   D:::::D     D:::::DI::::I    E:::::::::::::::E        !:::!!:::!
+        echo   D:::::D     D:::::DI::::I    E:::::::::::::::E        !:::!!:::!
+        echo   D:::::D     D:::::DI::::I    E::::::EEEEEEEEEE        !:::!!:::!
+        echo   D:::::D     D:::::DI::::I    E:::::E                  !!:!!!!:!!
+        echo   D:::::D    D:::::D I::::I    E:::::E       EEEEEE      !!!  !!! 
+        echo DDD:::::DDDDD:::::DII::::::IIEE::::::EEEEEEEE:::::E               
+        echo D:::::::::::::::DD I::::::::IE::::::::::::::::::::E      !!!  !!! 
+    echo D::::::::::::DDD   I::::::::IE::::::::::::::::::::E     !!:!!!!:!!
+    echo DDDDDDDDDDDDD      IIIIIIIIIIEEEEEEEEEEEEEEEEEEEEEE      !!!  !!! 
+                                                                  
+                                                                  
+                                                                  
+                                                                  
+                                                                  
+                                                                  
+                                                                  
+
+
+                                                     
+                                                     
